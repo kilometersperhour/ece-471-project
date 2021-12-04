69900dcb27882e5701510d74ad2f611472e89b4f
Miles Alasdair Martin <miles.martin@maine.edu>
2021-12-04 13:45:49 -0500

    Starting over. Beginnings of polar coordinates code

---
+++ solar_tracker.ino
@@ -0,0 +1,27 @@
+int sensorValue[9]; // photoresistor 0 and photoresistor 1 inputs
+int topLeft[2]; /
+int topCenter[2];
+int topRight[2];
+int midLeft[2];
+int midCenter[2];
+int midRight[2];
+int botLeft[2];
+int botCenter[2];
+int botRight[2];
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /* 
+   *  Serial interface over bluetooth?
+   *  Rotate slightly to 
+   *  Poll photoresistors
+   *    ioctl adc channel 0 annd 1 over adc
+   *    capture sensor readings
+   *  Compare sensor readings 
+   */
+}
